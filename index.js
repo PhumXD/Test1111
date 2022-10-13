5897ef5c398d592f3532f88f142ef1033015756a
PhumXD <91413751+PhumXD@users.noreply.github.com>
2022-10-13 22:19:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-let license = prompt("How long would you like to buy the license for use website?")
+let license = prompt("How long would you like to buy the license for use website? [Answer in days]")
 console.log("license")
+console.log(license*"70")
